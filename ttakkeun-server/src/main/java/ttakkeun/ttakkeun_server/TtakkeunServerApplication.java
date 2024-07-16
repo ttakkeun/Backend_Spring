73e1ce0c3cb8a5716201911fd09510f42e2144dc
0-x-14 <0x14@daum.net>
2024-07-16 15:52:06 +0900

    [Chore] 프로젝트 초기 세팅
    프로젝트 초기 세팅

---
+++ ttakkeun-server/src/main/java/ttakkeun/ttakkeun_server/TtakkeunServerApplication.java
@@ -0,0 +1,13 @@
+package ttakkeun.ttakkeun_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtakkeunServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtakkeunServerApplication.class, args);
+	}
+
+}
---
+++ ttakkeun-server/src/test/java/ttakkeun/ttakkeun_server/TtakkeunServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ttakkeun.ttakkeun_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtakkeunServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
