05109a362cad70a7402d080096ea3bcdd75d9e9a
0-x-14 <0x14@daum.net>
2024-08-05 00:45:45 +0900

    [Feat] 사용자 포인트 차감 API 구현
    사용자 포인트 차감 API 구현
    일단 memberId를 입력받는 방식으로 구현했으나 추후 로그인 정보 조회 방식으로 수정할 예정

--- src/main/java/ttakkeun/ttakkeun_server/controller/DiagnoseController.java
+++ src/main/java/ttakkeun/ttakkeun_server/controller/DiagnoseController.java
@@ -1,17 +1,16 @@
 package ttakkeun.ttakkeun_server.controller;
 
+import feign.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ttakkeun.ttakkeun_server.apiPayLoad.ApiResponse;
 import ttakkeun.ttakkeun_server.apiPayLoad.code.status.ErrorStatus;
 import ttakkeun.ttakkeun_server.apiPayLoad.code.status.SuccessStatus;
 import ttakkeun.ttakkeun_server.dto.GetMyPointResponseDTO;
+import ttakkeun.ttakkeun_server.dto.UpdateMyPointResponseDTO;
 import ttakkeun.ttakkeun_server.service.DiagnoseService;
 
 import java.util.NoSuchElementException;
@@ -26,7 +25,7 @@ public class DiagnoseController {
     private DiagnoseService diagnoseService;
 
     // memberId 임의로 입력받아서 조회하는 방식으로 구현함
-    // 추후 액세스 토큰으로 멤버 아이디 받아와서 조회하는 방식으로 수정 예정
+    // 추후 로그인 정보로 멤버 아이디 받아와서 조회하는 방식으로 수정 예정
     @GetMapping("/point")
     public ResponseEntity<ApiResponse<GetMyPointResponseDTO>> getPointsByMember(@RequestParam("member-id") Long memberId) {
         try {
@@ -35,9 +34,24 @@ public class DiagnoseController {
             return ResponseEntity.ok(response);
         } catch (NoSuchElementException e) {
             ApiResponse<GetMyPointResponseDTO> response = ApiResponse.ofFailure(ErrorStatus.MEMBER_NOT_FOUND, null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         } catch (Exception e) {
             ApiResponse<GetMyPointResponseDTO> response = ApiResponse.ofFailure(ErrorStatus._INTERNAL_SERVER_ERROR, null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @PatchMapping("/loading")
+    public ResponseEntity<ApiResponse<UpdateMyPointResponseDTO>> updatePointsByMember(@RequestParam("member-id") Long memberId) {
+        try {
+            Integer point = diagnoseService.updatePointsByMember(memberId);
+            ApiResponse<UpdateMyPointResponseDTO> response = ApiResponse.of(SuccessStatus._OK, new UpdateMyPointResponseDTO(point));
+            return ResponseEntity.ok(response);
+        } catch (NoSuchElementException e) {
+            ApiResponse<UpdateMyPointResponseDTO> response = ApiResponse.ofFailure(ErrorStatus.MEMBER_NOT_FOUND, null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        } catch (Exception e) {
+            ApiResponse<UpdateMyPointResponseDTO> response = ApiResponse.ofFailure(ErrorStatus._INTERNAL_SERVER_ERROR, null);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
