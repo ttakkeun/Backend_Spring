d3f2f4704d2dcbce316ac11cdd7b0608939440c1
riadan710 <riadan710@gmail.com>
2024-08-06 14:38:07 +0900

    [Fix] Like, Product 주석처리
    Like, Product 관련에서 오류 발생하여 전체 주석처리 (이후 수정해야함)

--- src/main/java/ttakkeun/ttakkeun_server/controller/ProductController.java
+++ src/main/java/ttakkeun/ttakkeun_server/controller/ProductController.java
@@ -1,42 +1,42 @@
-package ttakkeun.ttakkeun_server.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import ttakkeun.ttakkeun_server.dto.ProductApiResponseDTO;
-import ttakkeun.ttakkeun_server.dto.ProductDTO;
-import ttakkeun.ttakkeun_server.service.ProductService;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/product")
-@RequiredArgsConstructor
-public class ProductController {
-    private final ProductService productService;
-
-    //ai 추천 제품
-    @GetMapping("/ai")
-    public ResponseEntity<ProductApiResponseDTO> getAiProducts() {
-        try {
-            List<ProductDTO> products = productService.getResultProducts();
-            ProductApiResponseDTO response = ProductApiResponseDTO.builder()
-                    .isSuccess(true)
-                    .code(200)
-                    .message("성공")
-                    .result(products)
-                    .build();
-
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            ProductApiResponseDTO response = ProductApiResponseDTO.builder()
-                    .isSuccess(false)
-                    .code(500)
-                    .message("에러: " + e.getMessage())
-                    .build();
-
-            return ResponseEntity.status(500).body(response);
-        }
-    }
-}
-
+//package ttakkeun.ttakkeun_server.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import ttakkeun.ttakkeun_server.dto.ProductApiResponseDTO;
+//import ttakkeun.ttakkeun_server.dto.ProductDTO;
+//import ttakkeun.ttakkeun_server.service.ProductService;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/product")
+//@RequiredArgsConstructor
+//public class ProductController {
+//    private final ProductService productService;
+//
+//    //ai 추천 제품
+//    @GetMapping("/ai")
+//    public ResponseEntity<ProductApiResponseDTO> getAiProducts() {
+//        try {
+//            List<ProductDTO> products = productService.getResultProducts();
+//            ProductApiResponseDTO response = ProductApiResponseDTO.builder()
+//                    .isSuccess(true)
+//                    .code(200)
+//                    .message("성공")
+//                    .result(products)
+//                    .build();
+//
+//            return ResponseEntity.ok(response);
+//        } catch (Exception e) {
+//            ProductApiResponseDTO response = ProductApiResponseDTO.builder()
+//                    .isSuccess(false)
+//                    .code(500)
+//                    .message("에러: " + e.getMessage())
+//                    .build();
+//
+//            return ResponseEntity.status(500).body(response);
+//        }
+//    }
+//}
+//
--- src/main/java/ttakkeun/ttakkeun_server/service/LikeService.java
+++ src/main/java/ttakkeun/ttakkeun_server/service/LikeService.java
@@ -1,33 +1,33 @@
-package ttakkeun.ttakkeun_server.service;
-
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.relational.core.sql.Like;
-import org.springframework.stereotype.Service;
-import ttakkeun.ttakkeun_server.entity.Product;
-import ttakkeun.ttakkeun_server.repository.LikeRepository;
-import ttakkeun.ttakkeun_server.repository.ProductRepository;
-
-import java.util.List;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class LikeService {
-    private final LikeRepository likeRepository;
-    private final ProductRepository productRepository;
-
-    //제품의 좋아요 수 반환
-    public int getTotalLikes(Long productId) {
-        return likeRepository.findMembersByProductId(productId).size();
-    }
-
-    //사용자의 좋아요 판단
-    public Boolean getLikeStatus(Long productId) {
-
-        //임시 멤버값
-        Long memberId = 526L;
-        Product targetProduct = productRepository.findById(productId);
-        return likeRepository.findProductsByMemberId(memberId).contains(targetProduct);
-    }
-}
+//package ttakkeun.ttakkeun_server.service;
+//
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.relational.core.sql.Like;
+//import org.springframework.stereotype.Service;
+//import ttakkeun.ttakkeun_server.entity.Product;
+//import ttakkeun.ttakkeun_server.repository.LikeRepository;
+//import ttakkeun.ttakkeun_server.repository.ProductRepository;
+//
+//import java.util.List;
+//
+//@Service
+//@Transactional
+//@RequiredArgsConstructor
+//public class LikeService {
+//    private final LikeRepository likeRepository;
+//    private final ProductRepository productRepository;
+//
+//    //제품의 좋아요 수 반환
+//    public int getTotalLikes(Long productId) {
+//        return likeRepository.findMembersByProductId(productId).size();
+//    }
+//
+//    //사용자의 좋아요 판단
+//    public Boolean getLikeStatus(Long productId) {
+//
+//        //임시 멤버값
+//        Long memberId = 526L;
+//        Product targetProduct = productRepository.findById(productId);
+//        return likeRepository.findProductsByMemberId(memberId).contains(targetProduct);
+//    }
+//}
