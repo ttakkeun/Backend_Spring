8900d39f613522f154ca8383d9644cfa1bf92a0f
인태영 <intaeyoung@intaeyeong-ui-noteubug.local>
2024-08-19 23:26:27 +0900

    [Feat] ai 추천제품에 pet_id PathVariable 추가

--- src/main/java/ttakkeun/ttakkeun_server/repository/custom/CustomResultRepositoryImpl.java
+++ src/main/java/ttakkeun/ttakkeun_server/repository/custom/CustomResultRepositoryImpl.java
@@ -4,6 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import ttakkeun.ttakkeun_server.entity.Record;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,8 +17,9 @@ public class CustomResultRepositoryImpl implements CustomResultRepository {
     private EntityManager em;
 
     @Override
-    public Long findLatestResultId() {
-        return em.createQuery("SELECT r.id FROM Result r ORDER BY r.createdAt DESC", Long.class)
+    public Long findLatestResultId(List<Record> records) {
+        return em.createQuery("SELECT r.id FROM Result r WHERE r.record IN :records ORDER BY r.createdAt DESC", Long.class)
+                .setParameter("records", records)
                 .setMaxResults(1)
                 .getSingleResult();
     }
