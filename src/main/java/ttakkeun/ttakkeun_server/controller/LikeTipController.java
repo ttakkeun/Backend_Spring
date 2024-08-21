321ea78ccb20565a3067bf4e4a7c6858da2b54c4
sijin1017 <trsj1017@naver.com>
2024-08-21 10:48:31 +0900

    함수주석처리 추가 및 like 관련 주석해제

--- src/main/java/ttakkeun/ttakkeun_server/controller/LikeTipController.java
+++ src/main/java/ttakkeun/ttakkeun_server/controller/LikeTipController.java
@@ -1,37 +1,37 @@
-//package ttakkeun.ttakkeun_server.controller;
-//
-//import io.swagger.v3.oas.annotations.Operation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.web.bind.annotation.*;
-//import ttakkeun.ttakkeun_server.dto.tip.LikeTipResponseDTO;
-//import ttakkeun.ttakkeun_server.entity.Member;
-//import ttakkeun.ttakkeun_server.service.LikeTipService;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/tips")
-//public class LikeTipController {
-//
-//    private final LikeTipService likeTipService;
-//
-//    @Operation(summary = "팁 좋아요/취소 토글 API")
-//    @PatchMapping("/like/{tip_id}")
-//    public ResponseEntity<LikeTipResponseDTO> toggleTipLike(
-//            @PathVariable("tip_id") Long tipId,
-//            @AuthenticationPrincipal Member member) {
-//
-//        likeTipService.toggleLikeTip(tipId, member.getMemberId());
-//
-//        int totalLikes = likeTipService.getTotalTipLikes(tipId);
-//        boolean isLike = likeTipService.getTipLikeStatus(tipId, member.getMemberId());
-//
-//        LikeTipResponseDTO response = LikeTipResponseDTO.builder()
-//                .totalLikes(totalLikes)
-//                .isLike(isLike)
-//                .build();
-//
-//        return ResponseEntity.ok(response);
-//    }
-//}
+package ttakkeun.ttakkeun_server.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import ttakkeun.ttakkeun_server.dto.tip.LikeTipResponseDTO;
+import ttakkeun.ttakkeun_server.entity.Member;
+import ttakkeun.ttakkeun_server.service.LikeTipService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/tips")
+public class LikeTipController {
+
+    private final LikeTipService likeTipService;
+
+    @Operation(summary = "팁 좋아요/취소 토글 API")
+    @PatchMapping("/like/{tip_id}")
+    public ResponseEntity<LikeTipResponseDTO> toggleTipLike(
+            @PathVariable("tip_id") Long tipId,
+            @AuthenticationPrincipal Member member) {
+
+        likeTipService.toggleLikeTip(tipId, member.getMemberId());
+
+        int totalLikes = likeTipService.getTotalTipLikes(tipId);
+        boolean isLike = likeTipService.getTipLikeStatus(tipId, member.getMemberId());
+
+        LikeTipResponseDTO response = LikeTipResponseDTO.builder()
+                .totalLikes(totalLikes)
+                .isLike(isLike)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/ttakkeun/ttakkeun_server/service/LikeTipService.java
+++ src/main/java/ttakkeun/ttakkeun_server/service/LikeTipService.java
@@ -1,56 +1,56 @@
-//package ttakkeun.ttakkeun_server.service;
-//
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import ttakkeun.ttakkeun_server.entity.LikeTip;
-//import ttakkeun.ttakkeun_server.entity.Member;
-//import ttakkeun.ttakkeun_server.entity.Tip;
-//import ttakkeun.ttakkeun_server.repository.LikeTipRepository;
-//import ttakkeun.ttakkeun_server.repository.MemberRepository;
-//import ttakkeun.ttakkeun_server.repository.TipRepository;
-//
-//import java.util.Optional;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class LikeTipService {
-//
-//    private final LikeTipRepository likeTipRepository;
-//    private final TipRepository tipRepository;
-//    private final MemberRepository memberRepository;
-//
-//    // 좋아요 버튼 토글 기능
-//    public void toggleLikeTip(Long tipId, Long memberId) {
-//        Tip tip = tipRepository.findById(tipId)
-//                .orElseThrow(() -> new IllegalArgumentException("Tip not found with id: " + tipId));
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new IllegalArgumentException("Member not found with id: " + memberId));
-//
-//        Optional<LikeTip> tipLikeOpt = likeTipRepository.findByTipIdAndMemberId(tipId, memberId);
-//
-//        if (tipLikeOpt.isPresent()) {
-//            likeTipRepository.delete(tipLikeOpt.get());
-//        } else {
-//            LikeTip likeTip = new LikeTip(null, tip, member);
-//            likeTipRepository.save(likeTip);
-//        }
-//    }
-//
-//    // 좋아요 수 반환
-//    public int getTotalTipLikes(Long tipId) {
-//        Tip tip = tipRepository.findById(tipId)
-//                .orElseThrow(() -> new IllegalArgumentException("Tip not found with id: " + tipId));
-//        return likeTipRepository.countByTip(tip);
-//    }
-//
-//    // 좋아요 상태 반환
-//    public boolean getTipLikeStatus(Long tipId, Long memberId) {
-//        Tip tip = tipRepository.findById(tipId)
-//                .orElseThrow(() -> new IllegalArgumentException("Tip not found with id: " + tipId));
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new IllegalArgumentException("Member not found with id: " + memberId));
-//        return likeTipRepository.existsByTipAndMember(tip, member);
-//    }
-//}
+package ttakkeun.ttakkeun_server.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ttakkeun.ttakkeun_server.entity.LikeTip;
+import ttakkeun.ttakkeun_server.entity.Member;
+import ttakkeun.ttakkeun_server.entity.Tip;
+import ttakkeun.ttakkeun_server.repository.LikeTipRepository;
+import ttakkeun.ttakkeun_server.repository.MemberRepository;
+import ttakkeun.ttakkeun_server.repository.TipRepository;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikeTipService {
+
+    private final LikeTipRepository likeTipRepository;
+    private final TipRepository tipRepository;
+    private final MemberRepository memberRepository;
+
+    // 좋아요 버튼 토글 기능
+    public void toggleLikeTip(Long tipId, Long memberId) {
+        Tip tip = tipRepository.findById(tipId)
+                .orElseThrow(() -> new IllegalArgumentException("Tip not found with id: " + tipId));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Member not found with id: " + memberId));
+
+        Optional<LikeTip> tipLikeOpt = likeTipRepository.findByTipIdAndMemberId(tipId, memberId);
+
+        if (tipLikeOpt.isPresent()) {
+            likeTipRepository.delete(tipLikeOpt.get());
+        } else {
+            LikeTip likeTip = new LikeTip(null, tip, member);
+            likeTipRepository.save(likeTip);
+        }
+    }
+
+    // 좋아요 수 반환
+    public int getTotalTipLikes(Long tipId) {
+        Tip tip = tipRepository.findById(tipId)
+                .orElseThrow(() -> new IllegalArgumentException("Tip not found with id: " + tipId));
+        return likeTipRepository.countByTip(tip);
+    }
+
+    // 좋아요 상태 반환
+    public boolean getTipLikeStatus(Long tipId, Long memberId) {
+        Tip tip = tipRepository.findById(tipId)
+                .orElseThrow(() -> new IllegalArgumentException("Tip not found with id: " + tipId));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Member not found with id: " + memberId));
+        return likeTipRepository.existsByTipAndMember(tip, member);
+    }
+}
