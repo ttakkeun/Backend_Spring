ee7654bc47adf301b66e1a10264a7ba1b26d5f7b
ity0526 <ity0526@gmail.com>
2024-11-21 17:12:28 +0900

    [Feat] 팁 스크랩 토글 기능 추가

---
+++ src/main/java/ttakkeun/ttakkeun_server/repository/ScrapTipRepository.java
@@ -0,0 +1,18 @@
+package ttakkeun.ttakkeun_server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ttakkeun.ttakkeun_server.entity.LikeTip;
+import ttakkeun.ttakkeun_server.entity.Member;
+import ttakkeun.ttakkeun_server.entity.ScrapTip;
+import ttakkeun.ttakkeun_server.entity.Tip;
+
+import java.util.Optional;
+
+public interface ScrapTipRepository extends JpaRepository<ScrapTip, Integer> {
+
+    Optional<ScrapTip> findByTipAndMember(Tip tip, Member member);
+
+    boolean existsByTipAndMember(Tip tip, Member member);
+}
