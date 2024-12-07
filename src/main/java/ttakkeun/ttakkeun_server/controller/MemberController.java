de6003cdf9afe62de00a8697cd17724f6ead6d65
sijin1017 <trsj1017@naver.com>
2024-12-07 23:57:04 +0900

    회원정보 불러오기 dto 추가

--- src/main/java/ttakkeun/ttakkeun_server/controller/MemberController.java
+++ src/main/java/ttakkeun/ttakkeun_server/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ttakkeun.ttakkeun_server.apiPayLoad.ApiResponse;
 import ttakkeun.ttakkeun_server.apiPayLoad.code.status.SuccessStatus;
+import ttakkeun.ttakkeun_server.dto.MemberResponseDto;
 import ttakkeun.ttakkeun_server.entity.Member;
 import ttakkeun.ttakkeun_server.service.MemberService;
 
@@ -21,8 +22,15 @@ public class MemberController {
 
     @Operation(summary = "회원정보 조회 API")
     @GetMapping("/info")
-    public ApiResponse<Member> getMemberInfo(@AuthenticationPrincipal Member member) {
-        return ApiResponse.onSuccess(member);
+    public ApiResponse<MemberResponseDto> getMemberInfo(@AuthenticationPrincipal Member member) {
+
+        MemberResponseDto memberResponse = new MemberResponseDto(
+                member.getMemberId(),
+                member.getUsername(),
+                member.getEmail()
+        );
+
+        return ApiResponse.onSuccess(memberResponse);
     }
 
     @Operation(summary = "닉네임 수정 API")
