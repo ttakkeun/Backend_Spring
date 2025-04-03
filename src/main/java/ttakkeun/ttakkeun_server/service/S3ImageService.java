7e3c23136e185145f39ee3f799a8f599b9d54215
do-dop <pdy0207@naver.com>
2025-04-03 15:04:53 +0900

    test

--- src/main/java/ttakkeun/ttakkeun_server/service/S3ImageService.java
+++ src/main/java/ttakkeun/ttakkeun_server/service/S3ImageService.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.util.IOUtils;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
